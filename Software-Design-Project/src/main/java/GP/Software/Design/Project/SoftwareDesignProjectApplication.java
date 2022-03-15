d4365f48cf382cee08359bc9009f5e5b7f50dd03
Turnbulll <71480607+Turnbulll@users.noreply.github.com>
2022-03-15 15:04:13 +1100

    Main Project Upload

---
+++ Software-Design-Project/src/main/java/GP/Software/Design/Project/SoftwareDesignProjectApplication.java
@@ -0,0 +1,13 @@
+package GP.Software.Design.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareDesignProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareDesignProjectApplication.class, args);
+	}
+
+}
