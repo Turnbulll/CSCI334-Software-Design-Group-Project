2340b97c42ba58e2905bc040a49bb563ead3e99c
Turnbulll <71480607+Turnbulll@users.noreply.github.com>
2022-03-22 17:12:12 +1100

    Add files via upload

---
+++ GroupProject/src/main/java/CSCI334/GroupProject/GroupProjectApplication.java
@@ -0,0 +1,13 @@
+package CSCI334.GroupProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupProjectApplication.class, args);
+	}
+
+}
