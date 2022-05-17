681523dabf4c2f9fc41d92ea4b5da31e908a4291
samuelLkennedy <Skennedy349@gmail.com>
2022-05-17 14:42:04 +1000

    whatever bro
    just has a component will mount now

--- front-end/react-app/src/pages/PatientProfile.js
+++ front-end/react-app/src/pages/PatientProfile.js
@@ -1,20 +1,16 @@
 import { render } from '@testing-library/react'
+import {getUser, setUser} from '../App.js'
 import React from 'react'
-/*
-		
-		{working on patient page being not broken - leaving the other ones bc this is an experiment:P}
-    {this.state.userType === "Patient" ? <Link to={{
-      pathname: "/PatientHome",
-      state: {props}
-  }} 
-  /> : null }
-*/
 class PatientProfile extends React.Component {
+  componentWillMount = () =>{
+    this.setState({user: getUser()})
+}
   render(){
   return (
     <div className='main'>
       <h1>X's Profile</h1>
       {/*format rows and columns so it looks better*/}
+      <div>{this.state.user.name}</div>
       <table>
         <label>allergies:</label>
         <input type="text" name="user" id="user" />
@@ -25,6 +21,7 @@ class PatientProfile extends React.Component {
         <input type="text" name="user" id="user" />
         <label> description of physical condition:</label>
         <input type="text" name="user" id="user" />
+        
       </table>
     </div>
   )
