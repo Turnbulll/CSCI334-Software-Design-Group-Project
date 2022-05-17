6f232399fd99cf7563ce54d24d18d943887c943d
CactusPlatypus <70307680+CactusPlatypus@users.noreply.github.com>
2022-05-17 18:40:42 +1000

    Error text added to sign in

--- front-end/react-app/src/pages/SignIn.js
+++ front-end/react-app/src/pages/SignIn.js
@@ -18,7 +18,8 @@ class SignIn extends React.Component {
     constructor(props) {
         super(props);
         this.state = {User: null,
-                    userType: null};
+                    userType: null,
+                    error: ""};
   
       }
 
@@ -28,6 +29,11 @@ class SignIn extends React.Component {
        //get data from input
        var username = document.getElementById("user").value;
        var password = document.getElementById("password").value;
+
+       if (username === "" || password === ""){
+            this.callError("Missing Input");
+            return;
+       }
         
       //check data valid
        this.checkCredentials(username, password);
@@ -35,6 +41,8 @@ class SignIn extends React.Component {
 
 
     async checkCredentials(username, password){
+
+
         //fetch requests for all user types
         var req1 = Axios.get("http://localhost:8080/Patient/Name?name="+username);
         var req2 = Axios.get("http://localhost:8080/Doctor/Name?name="+username);
@@ -43,7 +51,7 @@ class SignIn extends React.Component {
         //fetch all the requests
         Axios.all([req1, req2, req3]).then(
             Axios.spread((...responses) => {
-
+            this.setState({userType: ""});
             const resp1 = responses[0];
             const resp2 = responses[1];
             const resp3 = responses[2];
@@ -59,6 +67,8 @@ class SignIn extends React.Component {
                     user = resp1.data[0];
                     this.setState({User: resp1.data[0],
                                     userType: resp1.data[0].userType});
+                }else{
+                    this.callError("Invalid Username or Password");
                 }
                
             }
@@ -72,6 +82,8 @@ class SignIn extends React.Component {
                     user = resp2.data[0];
                     this.setState({User: resp2.data[0],
                         userType: resp2.data[0].userType});
+                }else{
+                    this.callError("Invalid Username or Password");
                 }
             }
 
@@ -83,11 +95,12 @@ class SignIn extends React.Component {
                     user = resp3.data[0];
                     this.setState({User: resp3.data[0],
                         userType: resp3.data[0].userType});
+                }else{
+                    this.callError("Invalid Username or Password");
                 }
 
-            }   
-
-
+            }  
+                
 
             //work around for state async issue
                 if (userType === "Doctor"){
@@ -102,6 +115,7 @@ class SignIn extends React.Component {
 
                 setUser(user);
 
+
             })
         ).then((...responses) => {this.log()}).catch(errors => {
              // react on errors.
@@ -109,8 +123,8 @@ class SignIn extends React.Component {
         });
     }
 
-    componentDidMount(){
-
+    callError = (txt) =>{
+        this.setState({error: txt});
     }
 
   
@@ -140,8 +154,9 @@ class SignIn extends React.Component {
                         <input type="password" name="password" id="password"/>
 
                         <text className='SignUpLink'>Don't Have an account? <Link to ="/SignUp">Click here to Sign Up</Link></text>
-
+                        <x className='errorText'>{this.state.error}</x>
                     </form><br/><br/>
+
                     <button onClick={this.login}  onMouseDown={this.login} >Submit</button>
                 </div>
            </div>
