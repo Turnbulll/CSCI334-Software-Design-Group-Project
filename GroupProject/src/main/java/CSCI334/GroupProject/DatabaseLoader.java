9ec8abc398f2f04349532ca4a2c72f094d1b82bd
Turnbulll <71480607+Turnbulll@users.noreply.github.com>
2022-05-17 14:17:24 +1000

    TestData Added

--- GroupProject/src/main/java/CSCI334/GroupProject/DatabaseLoader.java
+++ GroupProject/src/main/java/CSCI334/GroupProject/DatabaseLoader.java
@@ -50,62 +50,57 @@ public class DatabaseLoader implements CommandLineRunner {
 	@Override
 	public void run(String... strings) throws Exception{
 		//memory allocation
-		doctors = new Doctor[3];
-		patients = new Patient[3];
-		pharmacists = new Pharmacist[3];
-		treatments = new Treatment[3];
-		prescriptions = new Prescription[3];
+		int noOfRecords = 100;
+		doctors = new Doctor[noOfRecords];
+		patients = new Patient[noOfRecords];
+		pharmacists = new Pharmacist[noOfRecords];
+		treatments = new Treatment[noOfRecords];
+		prescriptions = new Prescription[noOfRecords];
 
 		ArrayList<String> allergies = new ArrayList<String>();
 		ArrayList<String> reactions = new ArrayList<String>();
 		ArrayList<String> medicines = new ArrayList<String>();
 		
 		log.info("Treatments are being created...");
-		treatments[0] = new Treatment(allergies, reactions, medicines);
-		treatments[1] = new Treatment(allergies, reactions, medicines);
-		treatments[2] = new Treatment(allergies, reactions, medicines);
+		for(int i = 0; i<noOfRecords; i++) {
+			treatments[i] = new Treatment(allergies, reactions, medicines);
+		}
 		log.info("Adding treatments to the database...");
 		treatmentService.addNewTreatments(treatments);
 		log.info("Finished adding treatments to the database");
 		
 		log.info("Prescriptions are being created...");
-		prescriptions[0] = new Prescription("medicine1" , 1.0f, 3);
-		prescriptions[1] = new Prescription("medicine2" , 1.0f, 3);
-		prescriptions[2] = new Prescription("medicine3" , 1.0f, 3);
+		for(int i = 0; i<noOfRecords; i++) {
+			prescriptions[i] = new Prescription("medicine" + i , 1.0f, 3);
+		}
 		log.info("Adding prescriptions to the database...");
 		prescriptionService.addNewPrescriptions(prescriptions);
 		log.info("Finished adding prescriptions to the database");
 		
 		List<Prescription> emptyPrescriptions = new ArrayList<Prescription>();
-		//emptyPrescriptions.add(prescriptions[0]);
-		
 		log.info("Patients are arriving..");
-		patients[0] = new Patient("Patient1", "password", "Patient", treatments[0], emptyPrescriptions);
-		patients[1] = new Patient("Patient2", "password", "Patient", treatments[1], emptyPrescriptions);
-		patients[2] = new Patient("Patient3", "password", "Patient", treatments[2], emptyPrescriptions);
+		for(int i = 0; i<noOfRecords; i++) {
+			patients[i] = new Patient("Patient"+ i, "password", "Patient", treatments[i], emptyPrescriptions);
+		}
 		log.info("Adding patients to the database...");
 		patientService.addNewUsers(patients);
 		log.info("Finished adding patients to the database");
 		
 		log.info("Hiring doctors..");
-		doctors[0] = new Doctor("Doctor1", "password", "Doctor");
-		doctors[1] = new Doctor("Doctor2", "password", "Doctor");
-		doctors[2] = new Doctor("Doctor3", "password", "Doctor");
+		for(int i = 0; i<noOfRecords; i++) {
+			doctors[i] = new Doctor("Doctor"+i, "password", "Doctor");
+		}
 		log.info("Adding doctors to the database...");	
 		doctorService.addNewUsers(doctors);
 		log.info("Finished adding doctors to the database");
 		
 		log.info("Hiring pharmacists..");
-		pharmacists[0] = new Pharmacist("Pharmacist1", "password", "Pharmacist");
-		pharmacists[1] = new Pharmacist("Pharmacist2", "password", "Pharmacist");
-		pharmacists[2] = new Pharmacist("Pharmacist3", "password", "Pharmacist");
+		for(int i = 0; i<noOfRecords; i++) {
+			pharmacists[i] = new Pharmacist("Pharmacist"+i, "password", "Pharmacist");
+		}
 		log.info("Adding pharmacists to the database...");
 		pharmacistService.addNewUsers(pharmacists);
-		log.info("Finished adding pharmacists to the database");
-
-		//patientService.addPrescription(patients[0].getUserId(), prescriptions[0].getPrescriptionId());
-		//System.out.println(patients[0].toString());
-		
+		log.info("Finished adding pharmacists to the database");	
 	}
 	
 
