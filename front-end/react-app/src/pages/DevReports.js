7801c4f76676fa21234889765aed00e25f7548df
CactusPlatypus <70307680+CactusPlatypus@users.noreply.github.com>
2022-05-19 13:08:21 +1000

    added comments

--- front-end/react-app/src/pages/DevReports.js
+++ front-end/react-app/src/pages/DevReports.js
@@ -7,8 +7,7 @@ class DevReports extends React.Component {
     
     constructor(props) {
         super(props);
-        this.state = {user: null,
-                      userType: "",
+        this.state = {userType: "",
                       doctorCount: "",
                       patientCount: "",
                       pharmacistCount: "",
@@ -21,7 +20,7 @@ class DevReports extends React.Component {
 
 
     componentWillMount = () =>{
-        this.setState({user: getUser(),
+        this.setState({
                        userType: getUser().userType
         });
 
@@ -29,6 +28,7 @@ class DevReports extends React.Component {
     }
 
     preloadStats = () =>{
+      //get the counts from the backend
       //doctors
       Axios.get("http://localhost:8080/Report/Doctor").then(resp =>{
         const string = resp.data.replace(/[A-Za-z]/g, "");
@@ -59,84 +59,107 @@ class DevReports extends React.Component {
 
     }
 
+    //generates a report counting medicines
     generateMedicinesReport= () =>{
-
+      //get all scripts from backend
       Axios.get("http://localhost:8080/Prescription").then(resp =>
-      {
+      { 
+        //save script array
         const prescriptions = resp.data;
+        //create a new map
         var medicineMap = new Map();
 
+        //intitialise the report string
         var string = "====================================================================\n";
         string += "Count of total times each medicine is in a prescription \n";
         string += "Total prescriptions: " + this.state.prescriptionCount +"\n";
         string += "====================================================================\n";
 
+        //for eachh prescription
         prescriptions.forEach(element => {
+          //if the medicine is alrady in the map increment its value
             if (medicineMap.has(element.medicine)){
               medicineMap.set(element.medicine, medicineMap.get(element.medicine) + 1)
-            }else{
+            }else{ //if not add it to the map
               medicineMap.set(element.medicine, 1);
             }
         });
 
+        //for each element in the map
         medicineMap.forEach((value, key) => {
+          //add its medicine and count to the report string
           string += key + ": " + value + "\n";
         })
+        //send the report string with a marker
         string += "====================================================================\n";
        // console.log(string);
-
+        //create  & download the report file
        this.createFile(string, "Medicines Report");
 
       });
-
-
     }
 
+    //generate a report counting the allergies
     generateAllergiesReport = () =>{
+      //get treatments from backend
       Axios.get("http://localhost:8080/Treatment").then(resp =>
       {
-        
+        //insitialise output string
         var string = "====================================================================\n";
        
+        //create allergies map
         var allergiesMap = new Map();
+
+        //set allergies count to 0
         var allergiesCount = 0;
 
+        //get all treatments from backend
         const treatments = resp.data;
+
+        //for each treatment
         for (var i = 0; i < treatments.length; i++){
 
+          //if treatment had 0 allergies, skip step in loop
           if (treatments[i].allergies.length < 1){
             continue;
           }
-
+          
+          //temp array allergies
           var allergies = treatments[i].allergies;
 
+          //for each allergy
           for (var j = 0; j < allergies.length; j++){
-            //add allergy to string
+            //if allergy in map
             if (allergiesMap.has(allergies[j])){
+              //increment its value
               allergiesMap.set(allergies[j], allergiesMap.get(allergies[j]) + 1)
-            }else{
+            }else{//if not
+              //add it to map
               allergiesMap.set(allergies[j], 1);
             }
+
+            //increment allergies count
             allergiesCount++;
           }
 
         }
 
+        //add header to string
         string += "Common Allergies\n";
         string += "Total Unique Allergies: " + allergiesMap.size + "\n"; 
         string += "Total Allergies: " + allergiesCount + "\n";
         string += "====================================================================\n";
-      
+        
+        //add each allergy and its count to string
         allergiesMap.forEach((value, key) => {
           string += key + ": " + value + "\n";
         })
 
-
+        //add footer
         string += "====================================================================\n";
-        //console.log(allergiesMap);
-        this.createFile(string, "Allergy Report");
-
         
+        //create file
+        this.createFile(string, "Allergy Report");
 
       });
 
